eb97843d2c6c1a90c700687b2984cbcb07a41e7e
ф <>
2019-02-16 18:29:09 +0400

    Add Retrofit

--- app/src/main/java/com/example/umoriliapi/MainActivity.java
+++ app/src/main/java/com/example/umoriliapi/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.umoriliapi;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            loadFragment(BlankFragment.newInstance());
+        }
+    }
+
+    //Размещение фрагмента во фрейм
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
