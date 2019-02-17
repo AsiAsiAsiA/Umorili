37a95c72577128f469e918268e673d01846c0f32
ф <>
2019-02-17 12:41:02 +0400

    add libraries

--- app/src/main/java/com/example/umoriliapi/BlankFragment.java
+++ app/src/main/java/com/example/umoriliapi/BlankFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 
@@ -14,12 +15,12 @@ import androidx.fragment.app.Fragment;
  */
 public class BlankFragment extends Fragment {
 
-    public static BlankFragment newInstance() {
+    static BlankFragment newInstance() {
         return new BlankFragment();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank, container, false);
