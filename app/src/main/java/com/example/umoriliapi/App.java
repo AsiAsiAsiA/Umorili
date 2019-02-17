b3ebc3dd4a1e63d289ee16f04c75e5f98a2b0897
ф <>
2019-02-17 20:30:35 +0400

    fix Html

--- app/src/main/java/com/example/umoriliapi/App.java
+++ app/src/main/java/com/example/umoriliapi/App.java
@@ -5,14 +5,10 @@ import android.app.Application;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-/**
- * Created by misha on 31.10.2016.
- */
 
 public class App extends Application {
 
     private static UmoriliApi umoriliApi;
-    private Retrofit retrofit;
 
     public static UmoriliApi getApi() {
         return umoriliApi;
@@ -22,7 +18,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        retrofit = new Retrofit.Builder()
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://umorili.herokuapp.com")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
--- app/src/main/java/com/example/umoriliapi/PostsAdapter.java
+++ app/src/main/java/com/example/umoriliapi/PostsAdapter.java
@@ -1,5 +1,7 @@
 package com.example.umoriliapi;
 
+import android.os.Build;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,11 +9,9 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-/**
- * Created by misha on 31.10.2016.
- */
 
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> {
 
@@ -22,21 +22,14 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_item, parent, false);
         return new ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        PostModel post = posts.get(position);
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//            holder.post.setText(Html.fromHtml(post.getElementPureHtml(), Html.FROM_HTML_MODE_LEGACY));
-//        } else {
-//            holder.post.setText(Html.fromHtml(post.getElementPureHtml()));
-//        }
-        holder.post.setText(post.getElementPureHtml());
-        holder.site.setText(post.getSite());
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(posts.get(position));
     }
 
     @Override
@@ -54,6 +47,17 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
             super(itemView);
             post = itemView.findViewById(R.id.postitem_post);
             site = itemView.findViewById(R.id.postitem_site);
+            itemView.setTag(itemView);
+        }
+
+        void bind(final PostModel postModel) {
+            String source = postModel.getElementPureHtml();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                post.setText(Html.fromHtml(source, Html.FROM_HTML_MODE_LEGACY));
+            } else {
+                post.setText(Html.fromHtml(source));
+            }
+            site.setText(postModel.getSite());
         }
     }
 }
